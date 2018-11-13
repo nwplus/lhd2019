2250c90bfb24c9023606344ce7b01f9eae45bac3
lynnzhang7 <lynnzhang7@gmail.com>
2018-11-12 18:06:34 -0800

    added sponsors heading

--- web/components/local-hack-day/index.js
+++ web/components/local-hack-day/index.js
@@ -105,7 +105,8 @@ class LocalHackDay extends React.Component {
             <p className="fill-min-width margin-top-s">Still have questions? Feel free to email us at <a href="mailto:hello@nwplus.io">hello@nwplus.io</a> or shoot us a message on <a href="https://facebook.com/nwhacks">Facebook</a>.</p>
           </div>
 
-          <div className="sponsors flex jc-center dir-col pad-top-tera">
+          <h2 className="fill-width margin-bottom-s pad-sides-tera scale-pad-sides-laptop">Sponsors</h2>
+          <div className="sponsors flex jc-center dir-col pad-top-m">
             {getImage('cute-bear', bearCircle)}
             <p className="primary flex jc-center text-center">
               Stay tuned for sponsor updates!
