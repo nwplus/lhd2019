131f362f8c020b789440c2a49a853d119151421f
lynnzhang7 <lynnzhang7@gmail.com>
2018-11-12 18:15:48 -0800

    change pad sides to desktop

--- web/components/local-hack-day/index.js
+++ web/components/local-hack-day/index.js
@@ -84,14 +84,14 @@ class LocalHackDay extends React.Component {
         </div>
 
         <div className="second-panel flex dir-row jc-between pad-bottom-mega pad-sides-mega scale-pad-sides-laptop-none scale-row-tablet text-align-center">
-          <div className="flex dir-col pad-sides-giga scale-pad-sides-laptop scale-width-tablet" style={{ width: '32%' }}>
+          <div className="flex dir-col pad-sides-giga scale-pad-sides-desktop scale-width-tablet" style={{ width: '32%' }}>
             <img className="pad-top-xl" alt="illustration" src={secondPanelLearn} />
             <h4>Learn</h4>
             <p>Whether you&apos;re a hackathon veteran, or a first-timer,
               you&apos;ll learn lots from our brilliant mentors, workshops and fellow attendees.
             </p>
           </div>
-          <div className="flex dir-col pad-sides-giga scale-pad-sides-laptop scale-width-tablet" style={{ width: '32%' }}>
+          <div className="flex dir-col pad-sides-giga scale-pad-sides-desktop scale-width-tablet" style={{ width: '32%' }}>
             <img className="pad-top-xl" alt="illustration" src={secondPanelBuild} />
             <h4>Build</h4>
             <p>Build your passion project while contributing to building an inclusive,
@@ -99,7 +99,7 @@ class LocalHackDay extends React.Component {
               you&apos;re interested in!
             </p>
           </div>
-          <div className="flex dir-col pad-sides-giga scale-pad-sides-laptop scale-width-tablet" style={{ width: '32%' }}>
+          <div className="flex dir-col pad-sides-giga scale-pad-sides-desktop scale-width-tablet" style={{ width: '32%' }}>
             <img className="pad-top-xl" alt="illustration" src={secondPanelShare} />
             <h4>Share</h4>
             <p>Share your ideas and knowledge! But most importantly, share hackathon moments and
